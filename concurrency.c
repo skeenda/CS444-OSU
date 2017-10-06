9cef3b257b8c8f68d2b769741d9a396050ca526e
Alex Ruef <ruefa@oregonstate.edu>
2017-10-06 16:47:50 -0700

    working on queue size limit

--- concurrency.c
+++ concurrency.c
@@ -6,7 +6,8 @@
 #include <semaphore.h>
 #include <sys/queue.h>
 
-pthread_t tid[2];
+pthread_t prod_id[2];
+pthread_t con_id[2];
 sem_t mySem;
 
 struct event
@@ -19,6 +20,19 @@ struct event
 
 TAILQ_HEAD(head, event) head;
 
+int queue_size()
+{
+	struct event *np;
+	int size = 0;
+
+	for(np = head.tqh_first; np != NULL; np = np->entries.tqe_next)
+	{
+		size++;
+	}
+
+	return size;
+}
+
 void* producer(void *arg)
 {
 	sem_wait(&mySem);
@@ -27,20 +41,26 @@ void* producer(void *arg)
 	myEvent->num = genrand_int32() % 8 + 2;
 	myEvent->random_wait = 5;
 
+	printf("Producing: %d, %d\n", myEvent->num, myEvent->random_wait);
+
 	TAILQ_INSERT_TAIL(&head, myEvent, entries);
+
+	printf("size: %d\n", queue_size());
 
 	sem_post(&mySem);
 }
 
 void* consumer(void *arg)
 {
-	struct event *np;
-
+	struct event *consumed_event;
 	sem_wait(&mySem);
 
-	for(np = head.tqh_first; np != NULL; np = np->entries.tqe_next){
-		printf("%d, %d\n", np->num, np->random_wait);
-	}
+	consumed_event = head.tqh_first;
+	
+	printf("Consuming: %d, %d\n", consumed_event->num, consumed_event->random_wait);
+	TAILQ_REMOVE(&head, consumed_event, entries);
+
+	printf("Size: %d\n", queue_size());
 
 	sem_post(&mySem);
 }
@@ -48,19 +68,22 @@ void* consumer(void *arg)
 int main()
 {
 	TAILQ_INIT(&head);
-	
+
 	sem_init(&mySem, 0, 1);
 
 	init_genrand(time(NULL));
 
 	int i = 0;
 	while(i < 2){
-		pthread_create(&(tid[i]), NULL, &producer, NULL);
+		pthread_create(&(prod_id[i]), NULL, &producer, NULL);
 		i++;
 	}
 
-	pthread_t consumerID;
-	pthread_create(&consumerID, NULL, &consumer, NULL);
+	i = 0;
+	while(i < 2){
+		pthread_create(&(con_id[i]), NULL, &consumer, NULL);
+		i++;
+	}
 
 
 	for(i=0; i<1000000;i++){}
