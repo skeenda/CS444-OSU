2851d15212e18e0ed01bab7438cbc8376bd9362a
Alexander Ruef <ruefa@os2.engr.oregonstate.edu>
2017-10-06 12:18:03 -0700

    random number generation now in correct range

--- concurrency.c
+++ concurrency.c
@@ -7,37 +7,42 @@
 
 pthread_t tid[2];
 sem_t mySem;
-struct event event_buffer[32];
+//struct event event_buffer[32];
 
-struct event {
+struct event
+{
 	int num;
 	int random_wait;
 };
 
-void* doSomeThing(void *arg){
+
+void* doSomeThing(void *arg)
+{
 	sem_wait(&mySem);
 
 	struct event myEvent;
-	myEvent.num = genrand_int32();
-	myEvent.randomWait = 5;
-	
-	event_buffer
+	myEvent.num = genrand_int32() % 8 + 2;
+	myEvent.random_wait = 5;
 
+	printf("%d\n", myEvent.num);
+	
 	sem_post(&mySem);
 
 	int i=0;
-	for(i=0; i<(100000000000);i++);
 }
 
-int main(){
-
+int main()
+{
 	sem_init(&mySem, 0, 1);
+
+	init_genrand(time(NULL));
 
 	int i = 0;
 	while(i < 2){
 		pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
 		i++;
 	}
+
 
 	for(i=0; i<1000000;i++){}
 }
