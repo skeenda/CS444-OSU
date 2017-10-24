6dc157fc5469f79775fe66f696ae6e2f8e293cac
Alex Ruef <ruefa@oregonstate.edu>
2017-10-24 13:26:31 -0700

    Added first thread creation

--- concur2/pickupFork.c
+++ concur2/pickupFork.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+
+pthread_t philo_id[5];
+
+void* table(void *arg){
+	printf("I think therefore I am\n");
+}
+
+int main(){
+	pthread_create(&philo_id[0], NULL, &table, NULL);
+
+	pthread_join(philo_id[0], NULL);
+}
+
