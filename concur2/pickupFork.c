70f77cdc69194b1e7ee8e51a4d407b972a5a76d0
Alex Ruef <ruefa@oregonstate.edu>
2017-10-24 19:13:26 -0700

    added comments to pickupForks.c

--- concur2/pickupFork.c
+++ concur2/pickupFork.c
@@ -4,15 +4,17 @@
 #include <stdlib.h>
 #include <errno.h>
 
-pthread_t philo_id[5];
-sem_t forks[5];
+pthread_t philo_id[5]; /* holds thread ids for philosophers */
+sem_t forks[5]; /* semaphores representing the forks */
 
+/* struct to hold philospher details */
 struct philosopher{
 	char* name;
 	int left_fork;
 	int right_fork;
 };
 
+/* puts philosophers to sleep so they have a random amount of time to think */
 void think(struct philosopher *philo){
 	int wait_time;
 
@@ -22,21 +24,32 @@ void think(struct philosopher *philo){
 	sleep(wait_time);
 }
 
+/* teaches philosophers how to pickup forks
+ * teaches them how to share because they can't take other philosophers forks
+ */
 void get_forks(struct philosopher *philo){
 	int sem_value;
 	int got_forks = 1;
 
 	while(got_forks){
+		/* check if left fork is picked up
+		   continue looping if it is */
 		sem_getvalue(&forks[philo->left_fork], &sem_value);
 		if(sem_value == 1){
+			/* fork is available so pick it up */
 			sem_wait(&forks[philo->left_fork]);
 
+			/*check if right fork is picked up */
 			sem_getvalue(&forks[philo->right_fork], &sem_value);
 			if(sem_value == 1){
+				/* fork is available so pick it up */
 				sem_wait(&forks[philo->right_fork]);
+				/* both forks are up stop looping */
 				got_forks = 0;
 			}
 			else{
+				/* right fork can't be picked up
+ 				   must drop left fork to free it up */
 				sem_post(&forks[philo->left_fork]);
 			}
 		}
@@ -45,6 +58,7 @@ void get_forks(struct philosopher *philo){
 	printf("%s has picked up forks %d and %d\n", philo->name, philo->left_fork, philo->right_fork);
 }
 
+/* teaches philosophers to eat by giving them a random amount of time to eat */
 void eat(struct philosopher *philo){
 	int wait_time;
 
@@ -54,6 +68,7 @@ void eat(struct philosopher *philo){
 	sleep(wait_time);
 }
 
+/* teaches philosophers how to put down forks by incrementing semaphore */
 void put_forks(struct philosopher *philo){
 	sem_post(&forks[philo->left_fork]);
 	sem_post(&forks[philo->right_fork]);
@@ -61,6 +76,9 @@ void put_forks(struct philosopher *philo){
 	printf("%s put down forks %d and %d\n", philo->name, philo->left_fork, philo->right_fork);
 }
 
+/* table the philosophers are sitting around
+ * controls philosophers behavior
+ */
 void table(struct philosopher *philo){
 	while(1){
 		think(philo);
@@ -70,13 +88,19 @@ void table(struct philosopher *philo){
 	}
 }
 
+/* setup each philosophers struct
+ * their thread id is used to determine where they sit at the table
+ */
 void* preparePhilo(void *arg){
 	int i;
 	struct philosopher *philo;
 
 	philo = malloc(sizeof(struct philosopher));
+	/*figure out which thread is running this code*/
 	for(i=0; i < sizeof(philo_id)/sizeof(philo_id[0]); i++){
 		if(pthread_self() == philo_id[i]){
+			/*each philosopher has a unique name 
+ 			so they are setup separately*/
 			switch(i){
 				case 0:
 					philo->name = "Plato";
@@ -109,6 +133,7 @@ void* preparePhilo(void *arg){
 		}
 	}
 
+	/* philosopher is ready to sit at the table*/
 	table(philo);
 }
 
@@ -117,14 +142,17 @@ int main(){
 
 	srand(time(NULL));	/* Init random generator */
 
+	/* initialize semaphore forks */
 	for(i=0; i < sizeof(forks)/sizeof(forks[0]); i++){
 		sem_init(&forks[i], 0, 1);
 	}
 
+	/* spawn philosopher threads */
 	for(i=0; i < sizeof(philo_id)/sizeof(philo_id[0]); i++){
 		pthread_create(&philo_id[i], NULL, &preparePhilo, NULL);
 	}
 	
+	/* wait for threads to finish which is never */
 	for(i=0; i < sizeof(philo_id)/sizeof(philo_id[0]); i++){
 		pthread_join(philo_id[i], NULL);
 	}
