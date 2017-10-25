f9297114aae4d2b9ce35025002baa99dc0ac2bf1
Alex Ruef <ruefa@oregonstate.edu>
2017-10-24 19:00:09 -0700

    taught philosophers how to put down forks

--- concur2/pickupFork.c
+++ concur2/pickupFork.c
@@ -9,8 +9,8 @@ sem_t forks[5];
 
 struct philosopher{
 	char* name;
-	sem_t* left_fork;
-	sem_t* right_fork;
+	int left_fork;
+	int right_fork;
 };
 
 void think(struct philosopher *philo){
@@ -27,22 +27,22 @@ void get_forks(struct philosopher *philo){
 	int got_forks = 1;
 
 	while(got_forks){
-		sem_getvalue(philo->left_fork, &sem_value);
+		sem_getvalue(&forks[philo->left_fork], &sem_value);
 		if(sem_value == 1){
-			sem_wait(philo->left_fork);
+			sem_wait(&forks[philo->left_fork]);
 
-			sem_getvalue(philo->right_fork, &sem_value);
+			sem_getvalue(&forks[philo->right_fork], &sem_value);
 			if(sem_value == 1){
-				sem_wait(philo->right_fork);
+				sem_wait(&forks[philo->right_fork]);
 				got_forks = 0;
 			}
 			else{
-				sem_post(philo->left_fork);
+				sem_post(&forks[philo->left_fork]);
 			}
 		}
 	}
 
-	printf("%s has picked up forks\n", philo->name);
+	printf("%s has picked up forks %d and %d\n", philo->name, philo->left_fork, philo->right_fork);
 }
 
 void eat(struct philosopher *philo){
@@ -54,11 +54,19 @@ void eat(struct philosopher *philo){
 	sleep(wait_time);
 }
 
+void put_forks(struct philosopher *philo){
+	sem_post(&forks[philo->left_fork]);
+	sem_post(&forks[philo->right_fork]);
+
+	printf("%s put down forks %d and %d\n", philo->name, philo->left_fork, philo->right_fork);
+}
+
 void table(struct philosopher *philo){
 	while(1){
 		think(philo);
 		get_forks(philo);
 		eat(philo);
+		put_forks(philo);
 	}
 }
 
@@ -72,28 +80,28 @@ void* preparePhilo(void *arg){
 			switch(i){
 				case 0:
 					philo->name = "Plato";
-					philo->left_fork = &forks[0];
-					philo->right_fork = &forks[4];
+					philo->left_fork = 0;
+					philo->right_fork = 4;
 					break;
 				case 1:
 					philo->name = "Socrates";
-					philo->left_fork = &forks[1];
-					philo->right_fork = &forks[0];
+					philo->left_fork = 1;
+					philo->right_fork = 0;
 					break;
 				case 2:
 					philo->name = "Marx";
-					philo->left_fork = &forks[2];
-					philo->right_fork = &forks[1];
+					philo->left_fork = 2;
+					philo->right_fork = 1;
 					break;
 				case 3:
 					philo->name = "Nietzsche";
-					philo->left_fork = &forks[3];
-					philo->right_fork = &forks[2];
+					philo->left_fork = 3;
+					philo->right_fork = 2;
 					break;
 				case 4:
 					philo->name = "Machiavelli";
-					philo->left_fork = &forks[4];
-					philo->right_fork = &forks[3];
+					philo->left_fork = 4;
+					philo->right_fork = 3;
 					break;
 				default:
 					printf("An error occurred.\n");
