dae71bc30e9cc680a72eac9b7cf4562e7ff7dadf
David Skeen <skeenda@os2.engr.oregonstate.edu>
2017-10-29 15:36:18 -0700

    Looped the main so it will run until killed to generate a lot of IO

--- hw2/genFiles.py
+++ hw2/genFiles.py
@@ -1,19 +1,21 @@
 import os
-import random
+from random import randint
 
 
 def write(i):
-	f = open( str(i) + ".txt"", "a+")
+	f = open( str(i) + ".txt", "a+")
 	for x in range(randint(0, 50)):
 		x = randint(0,500)
-		f.write(x)
-		f.close()
+		f.write(str(x))
+	print ( "Wrote to:" + i)
+	f.close()
 
 if __name__ == "__main__":
-	fileNames = [test1,test2,test3,test4,test5]
-	for i in fileNames:
-		write(i)
-		f = open(str(i) + ".txt")
-		data = f.read()
-		f.close()
-		print (data + "\n")
+	fileNames = ["test1","test2","test3","test4","test5"]
+	while(True):
+		for i in fileNames:
+			write(i)
+			f = open(str(i) + ".txt")
+			data = f.read()
+			f.close()
+			print ("Read from: " + i + "\n")
