553bc03b66fdcf71a3276a1750c961aea10fd0a2
Alex Ruef <ruefa@oregonstate.edu>
2017-11-22 16:05:02 -0800

    creating searcher threads

--- concur3/concur3Pt2.c
+++ concur3/concur3Pt2.c
@@ -3,12 +3,24 @@
 #include <semaphore.h>
 
 pthread_t searchers[1];
-//pthread_t inserters[1];
-//pthread_t deleters[1];
+pthread_t inserters[1];
+pthread_t deleters[1];
 
 sem_t inserting;
 sem_t deleting;
 
-int main(){
+void* search(void *arg){
+	printf("I am a searcher.\n");
+}
 
+int main(){
+	int i;
+
+	for(i=0; i < sizeof(searchers)/sizeof(searchers[0]); i++){
+		pthread_create(&searchers[i], NULL, &search, NULL);
+	}
+
+	for(i=0; i < sizeof(searchers)/sizeof(searchers[0]); i++){
+		pthread_join(searchers[i], NULL);
+	}
 }
